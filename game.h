9f6ab5759c3ed04652f20859c5e36218fa508c47
Rintaro KOMATSU <rintaro4832@icloud.com>
2024-11-18 19:31:54 +0900

    ver2

--- game.h
+++ game.h
@@ -1,10 +1,14 @@
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
 #include <GL/glut.h>
+#include <vector>
 #include <iostream>
 #include "utility.h"
-
+#include "draw_mesh.h"
+#include "obstacle.h"
 
 void init_GL(int argc, char *argv[]);
 void init();
@@ -15,7 +19,7 @@ void glut_keyboard(unsigned char key, int x, int y);
 void glut_mouse(int button, int state, int x, int y);
 void glut_motion(int x, int y);
 void glut_idle();
-void draw_cube(double x_size , double y_size , double z_size , double* color );
+void glut_time(int value);
 void* game(void*args);
 
 // グローバル変数
@@ -26,18 +30,26 @@ bool g_isLeftButtonOn = false;
 bool g_isRightButtonOn = false;
 int n;
 int* eyes_state;
+int* right_eye_state;
+int* left_eye_state;
 
+
+obstacle_list obs_list = obstacle_list();
 
 typedef struct game_thread_args{
     int argc;
     char** argv;
     int *eye_state;
+	int *left_eye_state;
+	int *right_eye_state;
     pthread_mutex_t* eye_state_lock;
 }GameThreadArgs;
 
 void* game(void*args) {
     GameThreadArgs* game_args = (GameThreadArgs*)args;
 	eyes_state = game_args ->eye_state;
+	right_eye_state = game_args->right_eye_state;
+	left_eye_state = game_args->left_eye_state;
 	init_GL(game_args->argc, game_args->argv);
 	/* このプログラム特有の初期化 */
 	init();
@@ -47,8 +59,8 @@ void* game(void*args) {
 
 	/* メインループ */
 	glutMainLoop(); 
-	
-	return 0;
+	shm_cleanup();
+	return nullptr;
 }
 
 void init_GL(int argc, char *argv[]){
@@ -69,6 +81,7 @@ void set_callback_functions(){
     glutMotionFunc(glut_motion);
     glutPassiveMotionFunc(glut_motion);
 	glutIdleFunc(glut_idle);
+	glutTimerFunc(int(1000/FRAME_RATE), glut_time, 0);
 }
 
 void glut_keyboard(unsigned char key, int x, int y){
@@ -131,8 +144,11 @@ void glut_display(){
     glRotatef(-1*g_angle2, 1, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-	double color[3] = {1, 1, 1};
-    draw_cube(1, 2+ *eyes_state, 2, color);
+	double color[3] = {100, 1, 0.5};
+    draw_sphere(0,-3-*left_eye_state, 0, 0.9, color);
+	std::cout << -3-*left_eye_state << '\n';
+	draw_sphere(0,3+*right_eye_state, 0, 0.9, color);
+	obs_list.draw();
     glDisable(GL_DEPTH_TEST);
 
     glutSwapBuffers();
@@ -142,109 +158,18 @@ void glut_idle() {
 	glutPostRedisplay();
 }
 
-
-void hsv2rgb(double *hsv, double *rgb) {
-	double h = hsv[0], s = hsv[1], v = hsv[2];
-	double h0 = (fmod(h, 360))/60;
-	double c = s;
-	double x = c*(1-abs(fmod(h0, 2) -1));
-	double r = v-c, g = v-c, b = v-c;
-	switch (((int)h0))
-	{
-	case 0:
-		r += c;g+=x;
-		break;
-	case 1:
-		r +=x;g+=c;
-		break;
-	case 2:
-		g +=c;b+=x;
-		break;
-	case 3:
-		g+=x;b+=c;
-		break;
-	case 4:
-		r+=x;b+=c;
-		break;
-	case 5:
-		r+=c;b+=x;
-		break;
+void glut_time(int value) {
+	static int obs_interval_count = 0;
+	static double obs_interval = 1;//second
+	static int obs_interval_count_max = int(FRAME_RATE*obs_interval);
+	
+	if (obs_interval_count == obs_interval_count_max) {
+		obs_list.add(obstacle());
+		obs_list.del_obstacle();
+		obs_interval_count = 0;
 	}
-	rgb[0] = r; rgb[1] = g; rgb[2] = b;
-}
+	obs_interval_count ++;
+	obs_list.move();
 
-void setColorRGB(double *rgb) {
-	glColor3d(rgb[0], rgb[1], rgb[2]);
-}
-
-void setColorHSV(double *hsv) {
-	double rgb[3];
-	hsv2rgb(hsv, rgb);
-	setColorRGB(rgb);
-}
-
-void draw_cube(double x_size, double y_size , double z_size , double* color ) {
-	if (color == nullptr) {
-		double color0[3] = {1.0, 1.0, 1.0};
-		color = color0; 
-	}
-    double *hsv = color;
-    double x0 = x_size/2;
-    double y0 = y_size/2;
-    double z0 = z_size/2;
-	GLdouble pointA[] = {x0, y0, z0};
-	GLdouble pointB[] = {-1*x0, y0, z0};
-	GLdouble pointC[] = {-1*x0, -1*y0, z0};
-	GLdouble pointD[] = {x0, -1*y0, z0};
-    GLdouble pointE[] = {x0, y0, -1*z0};
-	GLdouble pointF[] = {-1*x0, y0, -1*z0};
-	GLdouble pointG[] = {-1*x0, -1*y0, -1*z0};
-	GLdouble pointH[] = {x0, -1*y0, -1*z0};
-
-    setColorHSV(hsv);
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointA);
-	glVertex3dv(pointB);
-	glVertex3dv(pointC);
-	glVertex3dv(pointD);
-	glEnd();
-
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointA);
-	glVertex3dv(pointB);
-	glVertex3dv(pointF);
-	glVertex3dv(pointE);
-	glEnd();
-
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointB);
-	glVertex3dv(pointC);
-	glVertex3dv(pointG);
-	glVertex3dv(pointF);
-	glEnd();
-
-    setColorHSV(hsv);
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointC);
-	glVertex3dv(pointD);
-	glVertex3dv(pointH);
-	glVertex3dv(pointG);
-	glEnd();
-
-    setColorHSV(hsv);
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointD);
-	glVertex3dv(pointA);
-	glVertex3dv(pointE);
-	glVertex3dv(pointH);
-	glEnd();
-
-    setColorHSV(hsv);
-    glBegin(GL_POLYGON);
-	glVertex3dv(pointE);
-	glVertex3dv(pointF);
-	glVertex3dv(pointG);
-	glVertex3dv(pointH);
-	glEnd();
-
+	glutTimerFunc(int(1000 / FRAME_RATE), glut_time, 0);
 }

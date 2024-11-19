ce4dd32655f4d63fe9d6662904e65385ca69e55e
Rintaro KOMATSU <rintaro4832@icloud.com>
2024-11-19 20:46:33 +0900

    ver4

---
+++ game_gl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cmath>
+#include <GL/glut.h>
+#include <vector>
+#include "utility.h"
+
+typedef struct {
+	std::vector<unsigned char> pixels;
+	int game_mode;
+    int score;
+	//pixels(WINDOW_X * WINDOW_Y * 3, 0) {}
+} args_game;
+
+void* thread_gl(void* args);
